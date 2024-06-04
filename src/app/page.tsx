b7b3fa59f532cb7b0fabef77fba007a53b8b836b
nktech-official <nk.technical.org@gmail.com>
2024-06-05 02:48:10 +0530

    fix:redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 import SignIn from "@/components/SignIn";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const userSession = sessionStorage.getItem("user");
+
+    if (userSession) router.push("/home");
+  });
   return (
     <main className="w-full min-h-screen">
       <SignIn />
